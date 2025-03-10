510e36d7ec0b8f63ffe8076f3f40bd753200da66
aabiuAlvarez <aabiujosmar@gmail.com>
2025-03-09 18:50:54 -0600

    Game

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.css'; 
+import 'sweetalert2';
+import Swal from 'sweetalert2';
+
+let player1 = new Game();
